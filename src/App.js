a8298734a6e2f4056a8a7285ab44c9bedb059a13
IKNManimendra <manimendraikn.19@uom.lk>
2022-03-30 10:24:49 +0530

    component part 1

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import './App.css';
+import Menu from './components/MenuComponent'
 
 function App() {
   return (
     <div className="App">
+
       <Navbar dark color="primary">
         <div className="container">
           <NavbarBrand href="/">Ristorente Con Fusion</NavbarBrand>
         </div>
       </Navbar>
+
+      <Menu />
     </div>
   );
 }
